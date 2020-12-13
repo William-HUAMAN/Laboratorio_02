91b6bcefddc91f9d67de8a435908a22f9addf0ee
Will-HH <whuaman@unitru.edu.pe>
2020-12-12 21:05:32 -0500

    Codigo con los comentarios agregados

--- WAR.py
+++ WAR.py
@@ -1,7 +1,7 @@
 import random
 from time import sleep as slp
 import os
-
+#Creamos una clase de carta para poder identificar los valores y tipo de carta
 class Carta:
     def __init__(self, valor, tipo):
         self.valor = valor
@@ -14,7 +14,7 @@ class Carta:
             return f"{self.valor} de {self.tipo}"
         else:
             return f"{nombres[self.valor-11]} de {self.tipo}"
-
+#Creamos una clase con metodos para un grupo de cartas para el juego
 class Grupo_cartas:
     def __init__(self, cartas=[]):
         self.cartas = cartas
@@ -36,7 +36,7 @@ class Grupo_cartas:
     def añadir_carta(self, carta_añadida):
         self.cartas.append(carta_añadida)
 
-
+#Creamos una clase de baraja para poder cumplir la funcion de una baraja
 class Baraja(Grupo_cartas):
     def __init__(self,cartas=[]):
         super().__init__(cartas)
@@ -57,14 +57,6 @@ player1=Grupo_cartas([])
 player2=Grupo_cartas([])
 
 
-#Mostramos las cantidades de cartas iniciales en la baraja y las que poseen los jugadores
-#print("Las cartas iniciales son:")
-#print(baraja_principal.verificar_cartas_restantes())
-#print(player1.verificar_cartas_restantes())
-#print(player2.verificar_cartas_restantes())
-#slp(2)
-#os.system("cls")
-
 #Repartimos las cartas de la baraja principal
 
 
@@ -75,22 +67,12 @@ for i in range(52):
     else:
         player2.añadir_carta(var)
     
-
-#Mostramos las cantidades de cartas luego de repartir en la baraja y las que poseen los jugadores
-
-#print("Las cartas finales son:")
-#print(baraja_principal.verificar_cartas_restantes())
-#print(player1.verificar_cartas_restantes())
-#print(player2.verificar_cartas_restantes())
-#slp(2)
-#os.system("cls")
-
-
+#Empezamos el juego 
 print("El juego ha empezado!!!!")
-#contador=0
+
 respuesta=""
 while player1.verificar_si_hay_cartas() and player2.verificar_si_hay_cartas() and respuesta!="no":
-    #contador+=1
+    
     input("Jugador 1, presione Enter para mostrar su carta")
     #Mostramos la carta del jugador 1
     carta_1=player1.mostar_carta()
@@ -105,7 +87,7 @@ while player1.verificar_si_hay_cartas() and player2.verificar_si_hay_cartas() an
     print("La carta del jugador 2 es: ",carta_2)
 
     print("...")
-
+    #Evaluamos los valores de las cartas y determinamos quien gana esa ronda
     if carta_1.valor>carta_2.valor:
         print("Jugador 1 se lleva las cartas")
         player1.añadir_carta(carta_1)
@@ -116,23 +98,24 @@ while player1.verificar_si_hay_cartas() and player2.verificar_si_hay_cartas() an
         player2.añadir_carta(carta_2)
     else:
         print("Se eliminan las cartas")
-
+    #Mostramos el número cde cartas que posee cada jugador
     print("Las cartas actuales son:")
     print("El jugador 1, posee: ",player1.verificar_cartas_restantes()," cartas.")
     print("El jugador 2, posee: ",player2.verificar_cartas_restantes()," cartas.")
 
-
+    #Preguntamos si el usuario desea terminar el juego
     respuesta=input("¿Desea continuar con el juego?, responda Si o No: ").lower()
     slp(1)
     os.system("cls")
 
 
 print("El juego ha finalizado")
+
+#Declaramos el ganador del juego
 if player1.verificar_cartas_restantes()<player2.verificar_cartas_restantes():
     print("El ganador es el jugador 2")
 elif player1.verificar_cartas_restantes()>player2.verificar_cartas_restantes():
     print("El ganador es el jugador 1")
 else:
     print("El juego ha terminado en empate")
-#print(contador)
 
